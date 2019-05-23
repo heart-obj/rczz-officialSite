20cb4afe5e534306560a72e6f71bbc5186a5fa22
newllb <1135769417@qq.com>
2019-05-23 09:38:02 +0800

    update

---
+++ src/views/index/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container-box">
+      <header class="header-box"></header>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Index',
+  data () {
+    return {}
+  }
+}
+</script>
+<style scoped>
+.container-box {
+ width: 100vmax;
+ background: red;
+}
+.header-box {
+  width: 100%;
+  height: 48.34vmax;
+}
+</style>
